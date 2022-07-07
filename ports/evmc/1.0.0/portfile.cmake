474be165f67e7ec2d184c054f9502c4bccd2ea50
monan <651932351@qq.com>
2022-07-07 21:33:42 +0800

    Update evmc & evmone

--- ports/evmc/1.0.0/portfile.cmake
+++ ports/evmc/1.0.0/portfile.cmake
@@ -1,15 +1,16 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO FISCO-BCOS/evmc
-    REF 2e8bc3a6fc9c3cdbf5440b329dcdf0753faee43c
-    SHA512 778810e2a18a64bdfcf3acc54cf7b1f782293cf29a6f0b5182f5f295db23c6d03e24f20d87381781a8d0b2ed5c26deb93fc76700392ffe0bc679e3649ea82254
+    REF 5bf9d9ffc326c7b0edb777208f5515107dd5106f
+    SHA512 d8f98fabd854641f12859fcdeb7b2197cfc7639c138e7ed375047176e807f2e4b8e2727d0ddf54b202690643356321a3764266a6c50e267042e0fbe42f5602ac
     HEAD_REF master
 )
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH} OPTIONS -DBUILD_SHARED_LIBS=OFF)
 vcpkg_cmake_build()
 vcpkg_cmake_install()
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
+
 vcpkg_cmake_config_fixup(CONFIG_PATH /lib/cmake/evmc)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
-vcpkg_fixup_pkgconfig()
