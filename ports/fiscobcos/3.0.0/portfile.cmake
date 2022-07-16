f59be508663daedd22adae4cb66991d1e15a3ef7
monan <651932351@qq.com>
2022-07-16 16:56:28 +0800

    Update fisco bcos port

--- ports/fiscobcos/3.0.0/portfile.cmake
+++ ports/fiscobcos/3.0.0/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO morebtcg/FISCO-BCOS-1
-    REF d1a118a599bffff2d96e15c5869f0defd0d2e3a4
-    SHA512 0686338c60516939b92c3007b3fb3dc86c410e00bbb056560ce8380f0043ae5c2e6ae06f24c01b9efb3961e2814a96a27cd5189484b7d1c45ab5736b984c4913
+    REF 33dabf104954cfbc3d9f5f9f80887190ae071209
+    SHA512 46ecc0a89e988d4efdbd3a06fb2c038e824403c1742d07d9409df6dba158bb9e23687a6396995fb425ebf8d970044b5a2ce40281b98e7ad53eb27a0394365fdd
     HEAD_REF feature-3.0.0
 )
 
@@ -11,6 +11,7 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
       "fullnode"     FULLNODE
       "lightnode"    WITH_LIGHTNODE
       "etcd"         WITH_ETCD
+      "tarsservices" WITH_TARS_SERVICES
 )
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH} OPTIONS ${FEATURE_OPTIONS})
@@ -18,4 +19,5 @@ vcpkg_cmake_build()
 vcpkg_cmake_install()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
