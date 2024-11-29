345ed35dd65c591797fff29e74e9e65a4d2b2415
cyjseagull <yujiechen_hust@163.com>
2024-11-29 17:07:22 +0800

    fix gsasl

--- ports/krb5/1.21.3/portfile.cmake
+++ ports/krb5/1.21.3/portfile.cmake
@@ -1,13 +1,9 @@
-vcpkg_download_distfile(ARCHIVE
-    URLS 
-        "https://github.com/krb5/krb5/archive/refs/tags/krb5-${VERSION}-final.tar.gz"
-    FILENAME "krb5-${VERSION}.tar.gz"
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO krb5/krb5
+    REF krb5-${VERSION}-final
     SHA512 82ea07acbc91a0352579aeec4f20d1c6d2c3f024640eaaa046f3ec5852d5d9c84143a1e0d04a5cf146f90c3406830823845eb608907f4060d7f765dfe0fb10a8
-)
-
-vcpkg_extract_source_archive(
-    SOURCE_PATH
-    ARCHIVE ${ARCHIVE}
+    HEAD_REF master
     PATCHES
         static-deps.diff
         define-des-zeroblock.diff
